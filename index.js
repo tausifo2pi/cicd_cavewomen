ba090d47b75ead2327838c5c2190e9f4ebe5e2d5
tausifo2pi <tausifo3.14@gmail.com>
2026-04-18 02:33:35 +0600

    done

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/health', (req, res) => {
 });
 
 app.use('/', (req, res) => {
-    res.json({ message: process.env.MESSAGE || 'Hello World' });
+    res.send(`<h1>${process.env.MESSAGE}</h1>`);
 });
 
 const port = process.env.PORT || 5000;
